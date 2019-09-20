aeeb0d3d0b94065af54bfffab53ef73ec07ef9d6
Kašpar <jan.kaspar@cgi.com>
2019-09-20 14:41:23 +0200

    fix bugs

--- test/unit/utils.test.ts
+++ test/unit/utils.test.ts
@@ -12,12 +12,24 @@ chai.use(chaiAsPromised);
 const expect = chai.expect;
 
 const portName = "/dev/TESTPORT";
-SerialPort.Binding = MockBinding;
-MockBinding.createPort(portName, { record: false });
 
 describe("utils.ts", () => {
 
     describe("connectBrick", () => {
+
+        beforeEach(() => {
+            SerialPort.Binding = MockBinding;
+            MockBinding.createPort(portName, {
+                echo: false,
+                readyData: Buffer.from([]),
+                record: true,
+            });
+        });
+
+        afterEach(() => {
+            MockBinding.reset();
+        });
+
         it("should return an EV3 Class Promise", async () => {
             // make the method always return a supported FW version
             sinon.stub(EV3.prototype, "getFWVersion").callsFake(async () => "V1.09H");
@@ -39,7 +51,7 @@ describe("utils.ts", () => {
         });
 
         it("should should throw if brick not found", () => {
-            expect(imp.findBrickPort([], "00112233aabb")).to.throw(Error);
+            expect(() => imp.findBrickPort([], "00112233aabb")).to.throw(Error);
         });
     });
 });
