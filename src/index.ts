edd53f73ca9dfd0012f1d48a7741101d9f2352ae
Kašpar <jan.kaspar@cgi.com>
2019-09-25 11:21:18 +0200

    merge utils into index.ts, add tests

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,36 @@
 import SerialPort from "serialport";
-import { connectBrick, findBrickPort } from "./utils";
+import { EV3 } from "./EV3";
 
-export async function connectBrickByPort(portName: string) {
-    return connectBrick(new SerialPort(portName, { autoOpen: false }));
+export { EV3, SerialPort };
+
+export async function connectBrickByPort(portName: string): Promise<EV3> {
+    return exports.connectBrick(new exports.SerialPort(portName, { autoOpen: false }));
 }
 
-export async function connectBrickById(brickId: string) {
-    return connectBrickByPort(findBrickPort(await SerialPort.list(), brickId));
+export async function connectBrickById(brickId: string): Promise<EV3> {
+    return exports.connectBrickByPort(exports.findBrickPort(await exports.SerialPort.list(), brickId));
+}
+
+export async function connectBrick(sp: SerialPort): Promise<EV3> {
+    const brick = new exports.EV3(sp);
+    await brick.connect();
+    const fwVersion = await brick.getFWVersion();
+    // tslint:disable-next-line: no-console
+    if (fwVersion !== "V1.09H") { console.warn(`Warning! This package is tested only on Firmware V1.09H. Your version (${fwVersion}) might not be supported.`); }
+    return brick;
+}
+
+export function findBrickPort(ports: SerialPort.PortInfo[], brickId: string): string {
+    const regex = RegExp(`\\&${brickId.toUpperCase()}\\_`);
+
+    const brickPort = ports.find((port) => {
+        if (port.pnpId === undefined) { return false; }
+        return regex.test(port.pnpId);
+    });
+
+    if (brickPort === undefined) {
+        throw new Error("Cannot find EV3 Brick with ID " + brickId);
+    } else {
+        return brickPort.comName;
+    }
 }
---
+++ test/index.test.ts
@@ -0,0 +1,104 @@
+import * as chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+import "mocha";
+import sinon from "sinon";
+import * as imp from "../src/index";
+
+import MockBinding from "@serialport/binding-mock";
+import SerialPort from "@serialport/stream";
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+const portName = "/dev/TESTPORT";
+
+describe("index.ts", () => {
+
+    beforeEach(() => {
+        SerialPort.Binding = MockBinding;
+        MockBinding.createPort(portName, {
+            echo: false,
+            readyData: Buffer.from([]),
+            record: true,
+        });
+    });
+
+    afterEach(() => {
+        MockBinding.reset();
+        sinon.restore();
+    });
+
+    describe("connectBrickByPort", () => {
+
+        it("should call the correct chain internally", async () => {
+            sinon.createStubInstance(imp.SerialPort);
+            const res = "foo";
+
+            const stub = sinon.stub(imp, "connectBrick").callsFake(async (sp) => {
+                expect(sp).to.be.instanceOf(imp.SerialPort);
+                return res as any;
+            });
+
+            await expect(imp.connectBrickByPort(portName)).to.eventually.equal(res);
+            expect(stub.called).to.equal(true);
+        });
+    });
+
+    describe("connectBrickById", () => {
+
+        it("should call the correct chain internally", async () => {
+            const res = "foo";
+
+            const stub1 = sinon.stub(imp.SerialPort, "list").callsFake(async () => {
+                return res as any;
+            });
+
+            const stub2 = sinon.stub(imp, "findBrickPort").callsFake((ports, brickId) => {
+                expect(ports).to.equal(res);
+                expect(brickId).to.equal(res);
+                return res as any;
+            });
+
+            const stub3 = sinon.stub(imp, "connectBrickByPort").callsFake(async (port) => {
+                expect(port).to.equal(res);
+                return res as any;
+            });
+
+            await expect(imp.connectBrickById(res)).to.eventually.equal(res);
+            expect(stub1.called).to.equal(true);
+            expect(stub2.called).to.equal(true);
+            expect(stub3.called).to.equal(true);
+        });
+    });
+
+    describe("connectBrick", () => {
+
+        it("should return an EV3 Class Promise", async () => {
+            // make the method always return a supported FW version
+            sinon.stub(imp.EV3.prototype, "getFWVersion").callsFake(async () => "V1.09H");
+
+            await expect(imp.connectBrick(new SerialPort(portName, { autoOpen: false }))).to.eventually.be.instanceOf(imp.EV3);
+        });
+
+        it("should print a console warning on unsupported FW");
+    });
+
+    describe("findBrickPort", () => {
+
+        it("should find a match on Generic Windows BT Serial Port", () => {
+            expect(imp.findBrickPort([
+                {
+                    comName: portName,
+                    pnpId: "BTHENUM\\{00001101-0000-1000-8000-00805F9B34FB}_LOCALMFG&0002\\7&D82B5DB&0&00112233AABB_C00000000",
+                },
+            ],
+            "00112233aabb")).to.equal(portName);
+        });
+
+        it("should should throw if brick not found", () => {
+            expect(() => imp.findBrickPort([
+                { comName: "testname" },
+            ], "00112233aabb")).to.throw(Error);
+        });
+    });
+});
